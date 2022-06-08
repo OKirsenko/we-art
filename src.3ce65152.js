d969c66d2c9fc596274fe1a1954c9e13e407a846
OKirsenko <79698389+OKirsenko@users.noreply.github.com>
2022-06-08 13:30:05 +0000

    Deploying to gh-pages from @ OKirsenko/we-art@7d59510f28dcfe6d54245c53d7350a7657bea86a 🚀

--- src.3ce65152.js
+++ src.3ce65152.js
@@ -1,6 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
 
-},{"./../fonts/CenturyGothic.eot":[["CenturyGothic.1c053ba1.eot","XxUu"],"XxUu"],"./../fonts/CenturyGothic.woff":[["CenturyGothic.a2f8ce23.woff","VpPX"],"VpPX"],"./../fonts/CenturyGothic.ttf":[["CenturyGothic.43a64ad3.ttf","UKyW"],"UKyW"]}],"Focm":[function(require,module,exports) {
-"use strict";require("./sass/main.scss");
-},{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/we-art/src.d4e8d330.js.map
+},{"./../fonts/CenturyGothic.eot":[["CenturyGothic.1c053ba1.eot","XxUu"],"XxUu"],"./../fonts/CenturyGothic.woff":[["CenturyGothic.a2f8ce23.woff","VpPX"],"VpPX"],"./../fonts/CenturyGothic.ttf":[["CenturyGothic.43a64ad3.ttf","UKyW"],"UKyW"],"./../images/pause.svg":[["pause.4eda269f.svg","Qd0i"],"Qd0i"],"./../images/play.svg":[["play.aa92ce98.svg","mOBW"],"mOBW"]}],"ZsLb":[function(require,module,exports) {
+const e=document.querySelector("#video"),t=document.querySelector(".video__controls"),a=t.querySelector("use");function l(a){t.classList.toggle("play"),"true"===e.dataset.play?(e.pause(),e.dataset.play="false"):(e.play(),e.dataset.play="true")}t.addEventListener("click",l);
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/controls");
+},{"./sass/main.scss":"clu1","./js/controls":"ZsLb"}]},{},["Focm"], null)
+//# sourceMappingURL=/we-art/src.3ce65152.js.map
